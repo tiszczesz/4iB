9191ad660686b88d7f8fbcd6dc709c6678aff1c0
fggg <you@example.com>
2022-09-12 14:16:16 +0200

    ff

--- cw1/zap1.sql
+++ cw1/zap1.sql
@@ -2,3 +2,16 @@ SELECT count(*) FROM `miejsca`;
 SELECT count(*) FROM uczestnicy;
 SELECT concat(uczestnicy.nazwisko,' ',uczestnicy.imie) 
      FROM uczestnicy;
+SELECT concat(uczestnicy.nazwisko,' ',
+         left(uczestnicy.imie,1),'.') 
+     FROM uczestnicy;
+SELECT length('Żółta');
+SELECT char_length('Żółta');
+SELECT nazwisko FROM uczestnicy ORDER BY nazwisko desc;
+SELECT nazwisko FROM uczestnicy ORDER BY char_length(nazwisko) desc;
+SELECT nazwisko, imie,char_length(concat(nazwisko,imie)) as `długość`
+    FROM uczestnicy 
+        ORDER BY char_length(concat(nazwisko,imie)) desc;
+SELECT nazwisko, imie,char_length(concat(nazwisko,imie)) as `długość`
+    FROM uczestnicy 
+        ORDER BY 3 desc;
