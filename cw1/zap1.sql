a1ee074d80f72b52f9fe0c95aadc71bdcbbeaa3a
fggg <you@example.com>
2022-09-19 14:35:56 +0200

    ff

--- cw1/zap1.sql
+++ cw1/zap1.sql
@@ -15,3 +15,35 @@ SELECT nazwisko, imie,char_length(concat(nazwisko,imie)) as `długość`
 SELECT nazwisko, imie,char_length(concat(nazwisko,imie)) as `długość`
     FROM uczestnicy 
         ORDER BY 3 desc;
+
+SELECT nazwa,cena, month(data_wyjazdu) from miejsca
+
+SELECT Time(data_wyjazdu) FROM `miejsca`
+
+SELECT count(*), month(data_wyjazdu) as `miesiąc` 
+        FROM `miejsca` GROUP BY month(data_wyjazdu)
+
+SELECT count(*), month(data_wyjazdu) as `miesiąc` FROM `miejsca` 
+ GROUP BY month(data_wyjazdu) ORDER by `miesiąc` DESC
+
+ SELECT count(*) as `ilość`, month(data_wyjazdu) as `miesiąc` FROM `miejsca`
+  GROUP BY month(data_wyjazdu) ORDER by `ilość` DESC
+
+  SELECT nazwa, cena ,data_wyjazdu,DAYOFYEAR(data_wyjazdu) as `dzień roku` from miejsca
+
+  SELECT nazwa, cena ,data_wyjazdu from miejsca WHERE DAYOFMONTH(data_wyjazdu)<=10
+
+  SELECT nazwa, cena ,data_wyjazdu from miejsca 
+  WHERE DAYOFMONTH(data_wyjazdu)   BETWEEN 10 and 15
+
+  SELECT nazwa, cena, data_wyjazdu, Year(Now())-Year(data_wyjazdu) as `lata`
+        FROM `miejsca` 
+
+SELECT nazwa, cena, data_wyjazdu FROM `miejsca` WHERE Year(data_wyjazdu) % 2 = 0
+
+SELECT nazwa, cena, data_wyjazdu
+FROM `miejsca`
+WHERE Year(data_wyjazdu) % 2 = 0 
+and month(data_wyjazdu) % 2 = 0
+
+SELECT TIMESTAMPDIFF(month,now(),'2021-04-12')
