3788d038230c86b6917e804844f2402bcbc2ba6d
fggg <you@example.com>
2022-10-17 14:02:33 +0200

    nn

--- cw2/cw2.php
+++ cw2/cw2.php
@@ -4,16 +4,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="cw2.css">
     <title>Wycieczki</title>
 </head>
 <body>
     <h1>Wykaz miejsc wycieczkowych</h1>
-    <pre>
+    <!-- <pre> -->
     <?php
     require_once "functions.php";
     $conn = getConnection();
     $d = getMiejsca();
+    // var_dump($d);
+    echo toList($d,"ol");
     ?>
-    </pre>
+    <!-- </pre> -->
 </body>
 </html>
--- cw2/functions.php
+++ cw2/functions.php
@@ -13,7 +13,18 @@ function getMiejsca():array
     if($conn==null) $dane;
     $sql = 'SELECT * FROM miejsca';
     $result = $conn->query($sql);
-    var_dump($result);
-
+    // var_dump($result);
+    while($row = $result->fetch_assoc()){
+        $dane[] = $row;
+    }
     return $dane;
+}
+function toList(array $data,string $kind="ul"):string{
+    $html = "<{$kind}>";
+    foreach($data as $row){
+        $html .= "<li>wycieczka do: <span class='green'>{$row["nazwa"]}</span> w cenie <span class='red'>{$row["cena"]}</span> PLN"
+             ." w dniu: <span class='grey'>{$row["data_wyjazdu"]}</span></li> ";
+    }
+
+    return $html . "</{$kind}>";
 }
