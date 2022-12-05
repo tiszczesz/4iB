6a0bce2353871d0deac13644a3c13062e0b8a878
fggg <you@example.com>
2022-12-05 14:06:36 +0100

    gg

--- ark1/zdanie/kwerendy.sql
+++ ark1/zdanie/kwerendy.sql
@@ -9,3 +9,10 @@ on uzytkownicy.id=dane.id
 where uzytkownicy.login like 'Justyna';
 
 alter TABLE dane add COLUMN stanowisko varchar(50) null
+
+
+
+select uzytkownicy.login, 
+  dane.rok_urodz,(Year(CURDATE())- dane.rok_urodz) as wiek
+from uzytkownicy INNER JOIN dane
+on uzytkownicy.id=dane.id 
